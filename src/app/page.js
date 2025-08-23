bf25aecebba1fd5b90a43e12335d80b81e3765a5
joscript <yosefeid058@gmail.com>
2025-08-23 23:55:28 +0300

    new-project

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,8 @@
 'use client'
-
-import { useEffect } from "react"
-import Header from "./components/header"
-
 export default function Home() {
-
-  useEffect(()=>{},[])
 
   return (
     <div className='w-full flex justify-center items-center bg-[#FDF8EE]'>
-      <Header />
 
     </div>
   )
